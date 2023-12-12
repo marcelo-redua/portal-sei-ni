b3dc86f697eb1c87f81ace9b4cc31e07438e5e6f
marcelo-redua <marcelo.redua@outlook.com>
2023-12-12 14:22:06 -0300

    projeto iniciado

---
+++ js/variaveldata.js
@@ -0,0 +1,11 @@
+// Cria um novo objeto Date
+var dataAtual = new Date();
+
+// Obtém o ano atual
+var anoAtual = dataAtual.getFullYear();
+
+// Define a variável de direitos autorais
+var direitosAutorais = "© " + anoAtual + " Prefeitura de Nova Iguaçu.";
+
+// Exibe a variável de direitos autorais
+console.log(direitosAutorais);
