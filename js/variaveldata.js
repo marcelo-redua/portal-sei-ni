f4e473cc257edc64a112e2f2d9f9c236577f120b
Jackson Augusto <jacksonaugusto@pmni>
2023-12-14 17:00:14 -0300

    adicionado o script responsavel pela atualização da data do copyright + modificada a imagem .svg novaiguaçu

--- js/variaveldata.js
+++ js/variaveldata.js
@@ -7,5 +7,6 @@ var anoAtual = dataAtual.getFullYear();
 // Define a variável de direitos autorais
 var direitosAutorais = "© " + anoAtual + " Prefeitura de Nova Iguaçu.";
 
-// Exibe a variável de direitos autorais
-console.log(direitosAutorais);
+function displayCopyright() {
+    document.getElementById("copyright").innerHTML = direitosAutorais;
+}
